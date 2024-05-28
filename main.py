84b9d7f5a105479b282935acbf593080c48e31de
hanlunli <hanlunli.007@gmail.com>
2024-05-28 13:25:23 -0700

    adsflkj

--- main.py
+++ main.py
@@ -13,18 +13,11 @@ from __init__ import app, db, cors  # Definitions initialization
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
-from api.titanic import titanic_api
-from api.badminton import badminton_api
-from api.loan import loan_api
-from api.heart import heart_api
-from api.sleep import sleep_api
-from api.water import water_api
 
 
 # database migrations
 from model.users import initUsers
 from model.players import initPlayers
-from model.titanicML import initTitanic
 
 # setup App pages
 
@@ -35,13 +28,6 @@ db.init_app(app)
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
-app.register_blueprint(sleep_api)
-app.register_blueprint(titanic_api) # register api routes
-app.register_blueprint(heart_api)
-app.register_blueprint(loan_api)
-app.register_blueprint(water_api)
-app.register_blueprint(badminton_api) # register app pages
-
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -64,8 +50,6 @@ custom_cli = AppGroup('custom', help='Custom commands')
 def generate_data():
     initUsers()
     initPlayers()
-    initTitanic()
-
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
 # this runs the application on the development server
